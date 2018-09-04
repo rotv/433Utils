e28b9a1d18219d96fb6eef7aabc04ba28d80fc50
Robert ter Vehn <robert@tervehn.se>
2018-09-05 00:03:45 +0200

    Added/improved help text and fixed indentation

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -1,65 +1,64 @@
 /*
-  RFSniffer
+Usage: ./RFSniffer [pulselength]
+pulselength    - pulselength in microseconds
 
-  Usage: ./RFSniffer [<pulseLength>]
-  [] = optional
-
-  Hacked from http://code.google.com/p/rc-switch/
-  by @justy to provide a handy RF code sniffer
+Hacked from http://code.google.com/p/rc-switch/
+by @justy to provide a handy RF code sniffer
 */
-
 #include "../rc-switch/RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-     
-     
-RCSwitch mySwitch;
- 
 
+RCSwitch mySwitch;
 
 int main(int argc, char *argv[]) {
-  
-     // This pin is not the first pin on the RPi GPIO header!
-     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-     // for more information.
-     int PIN = 2;
-     
-     if(wiringPiSetup() == -1) {
-       printf("wiringPiSetup failed, exiting...");
-       return 0;
-     }
 
-     int pulseLength = 0;
-     if (argv[1] != NULL) pulseLength = atoi(argv[1]);
+	// This pin is not the first pin on the RPi GPIO header!
+	// Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+	// for more information.
+	int PIN = 2;
 
-     mySwitch = RCSwitch();
-     if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
-     mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
-     
-    
-     while(1) {
-  
-      if (mySwitch.available()) {
-    
-        int value = mySwitch.getReceivedValue();
-    
-        if (value == 0) {
-          printf("Unknown encoding\n");
-        } else {    
-   
-          printf("Received %i\n", mySwitch.getReceivedValue() );
-        }
-    
-        fflush(stdout);
-        mySwitch.resetAvailable();
-      }
-      usleep(100); 
-  
-  }
+	// If wrong number of arguments or argument -h/--help is given, print the help text
+	if (argc > 2 || argc == 2 && 
+			(!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+			) {
+		printf("Usage: %s [pulselength]\n", argv[0]);
+		printf("Listens to RF signals and outputs any received commands as decimal codes.\n");
+		printf("\n");
+		printf("pulselength - optional. pulselength in microseconds\n");
+		return -1;
+	}
 
-  exit(0);
+	if(wiringPiSetup() == -1) {
+		printf("wiringPiSetup failed, exiting...");
+		return 0;
+	}
+
+	int pulseLength = 0;
+	if (argv[1] != NULL) pulseLength = atoi(argv[1]);
+
+	mySwitch = RCSwitch();
+	if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
+	mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
 
 
+	while(1) {
+		if (mySwitch.available()) {
+			int value = mySwitch.getReceivedValue();
+
+			if (value == 0) {
+				printf("Unknown encoding\n");
+			} else {    
+				printf("Received %i\n", mySwitch.getReceivedValue() );
+			}
+
+			fflush(stdout);
+			mySwitch.resetAvailable();
+		}
+		usleep(100); 
+	}
+
+	exit(0);
 }
 
--- RPi_utils/codesend.cpp
+++ RPi_utils/codesend.cpp
@@ -1,15 +1,17 @@
 /*
 Usage: ./codesend decimalcode [protocol] [pulselength]
-decimalcode - As decoded by RFSniffer
-protocol    - According to rc-switch definitions
-pulselength - pulselength in microseconds
+Sends the given decimalcode as an RF command.
 
- 'codesend' hacked from 'send' by @justy
- 
- - The provided rc_switch 'send' command uses the form systemCode, unitCode, command
-   which is not suitable for our purposes.  Instead, we call 
-   send(code, length); // where length is always 24 and code is simply the code
-   we find using the RF_sniffer.ino Arduino sketch.
+decimalcode - As decoded by RFSniffer
+protocol    - optional. According to rc-switch definitions
+pulselength - optional. pulselength in microseconds
+
+'codesend' hacked from 'send' by @justy
+
+- The provided rc_switch 'send' command uses the form systemCode, unitCode, command
+which is not suitable for our purposes.  Instead, we call 
+send(code, length); // where length is always 24 and code is simply the code
+we find using the RF_sniffer.ino Arduino sketch.
 
 (Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter 
 or your remote control)
@@ -19,39 +21,43 @@ or your remote control)
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    
-    // This pin is not the first pin on the RPi GPIO header!
-    // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-    // for more information.
-    int PIN = 0;
-    
-    // Parse the first parameter to this command as an integer
-    int protocol = 0; // A value of 0 will use rc-switch's default value
-    int pulseLength = 0;
 
-    // If no command line argument is given, print the help text
-    if (argc == 1) {
-        printf("Usage: %s decimalcode [protocol] [pulselength]\n", argv[0]);
-        printf("decimalcode\t- As decoded by RFSniffer\n");
-        printf("protocol\t- According to rc-switch definitions\n");
-        printf("pulselength\t- pulselength in microseconds\n");
-        return -1;
-    }
+	// This pin is not the first pin on the RPi GPIO header!
+	// Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+	// for more information.
+	int PIN = 0;
 
-    // Change protocol and pulse length accroding to parameters
-    int code = atoi(argv[1]);
-    if (argc >= 3) protocol = atoi(argv[2]);
-    if (argc >= 4) pulseLength = atoi(argv[3]);
-    
-    if (wiringPiSetup () == -1) return 1;
-    printf("sending code[%i]\n", code);
-    RCSwitch mySwitch = RCSwitch();
-    if (protocol != 0) mySwitch.setProtocol(protocol);
-    if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
-    mySwitch.enableTransmit(PIN);
-    
-    mySwitch.send(code, 24);
-    
-    return 0;
+	// Parse the first parameter to this command as an integer
+	int protocol = 0; // A value of 0 will use rc-switch's default value
+	int pulseLength = 0;
+
+	// If no command line argument is given or -h/--help is given, print the help text
+	if (argc > 2 || argc == 2 && 
+			(!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+			) {
+		printf("Usage: %s decimalcode [protocol] [pulselength]\n", argv[0]);
+		printf("Sends the given decimalcode as an RF command.\n");
+		printf("\n");
+		printf("decimalcode - As decoded by RFSniffer\n");
+		printf("protocol    - optional. According to rc-switch definitions\n");
+		printf("pulselength - optional. pulselength in microseconds\n");
+		return -1;
+	}
+
+	// Change protocol and pulse length accroding to parameters
+	int code = atoi(argv[1]);
+	if (argc >= 3) protocol = atoi(argv[2]);
+	if (argc >= 4) pulseLength = atoi(argv[3]);
+
+	if (wiringPiSetup () == -1) return 1;
+	printf("sending code[%i]\n", code);
+	RCSwitch mySwitch = RCSwitch();
+	if (protocol != 0) mySwitch.setProtocol(protocol);
+	if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
+	mySwitch.enableTransmit(PIN);
+
+	mySwitch.send(code, 24);
+
+	return 0;
 
 }
